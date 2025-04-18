09748d4148e277afd5e10243dda6cfd6623e57b1
yep_zane <zane.56909@gmail.com>
2025-04-18 15:21:34 +0100

    Create Tannoy - Product Loader.lua

---
+++ Source/Tannoy/Tannoy - Product Loader.lua
@@ -0,0 +1,29 @@
+ziton = [[
+          $$\   $$\                             
+          \__|  $$ |                            
+$$$$$$$$\ $$\ $$$$$$\    $$$$$$\  $$$$$$$\      
+\____$$  |$$ |\_$$  _|  $$  __$$\ $$  __$$\     
+  $$$$ _/ $$ |  $$ |    $$ /  $$ |$$ |  $$ |    
+ $$  _/   $$ |  $$ |$$\ $$ |  $$ |$$ |  $$ |    
+$$$$$$$$\ $$ |  \$$$$  |\$$$$$$  |$$ |  $$ |$$\ 
+\________|\__|   \____/  \______/ \__|  \__|\__|
+ 2025 - Ziton Systems
+ Tannoy System 
+ Updated 18.04.25
+]]
+
+Whitelist = {
+	Enabled = false,
+	Group = 33356595, -- GroupID 
+	IDs = {}, -- Group Ranks to apply to, put 'all' to do all.
+	Users = {'yep_zane',} -- Supports UserIDs and names/displaynames
+}
+Volume = 0.5
+Songs = { -- Copy and paste and change ID to SoundID and Name to the name you want.
+	{ID = 'rbxassetid://1848354536', Name = 'Example'},
+	--{ID = 0, Name = ''},
+	
+}
+
+--[[  DO NOT EDIT BELOW  ]]--
+module = require(71603876300548) module('291-657-282-849', true, script, Whitelist, Volume, Songs)
