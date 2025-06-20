519e83d8169e06c6c3993026ff2df0807aed2b87
yep_zane <zane.56909@gmail.com>
2025-06-20 21:53:49 +0100

    Create Infomation.lua

---
+++ Packages/0XP01/Infomation.lua
@@ -0,0 +1,13 @@
+--// This is intended to be a module script under the 'Loader.lua' file
+
+local module = {
+Whitelist = {
+		Enabled = false,
+		Group = 000000, -- GroupID 
+		IDs = {}, -- Group Ranks to apply to, put 'all' to do all.
+		Users = {} -- Supports UserIDs and names/displaynames
+}	
+	
+}
+
+return module
